06d832a78e6c2c3336c84d878055b809dd6710b9
joseriera97 <josealbertoriera@paucasesnovescifp.cat>
2020-10-07 20:38:11 +0200

    Se ha hecho un Form con dos botones distintos El primero es para OK El segundo es para Cancel

---
+++ ProyectButton/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK.\n    Perfect");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cancel.\n    Okey");
+
+        }
+    }
+}
